6fe3945eec418800b48b5641928d0e43a3dc606a
polotenchik <yashikpolotenchika@gmail.com>
2021-06-19 23:14:51 +0300

    calc

---
+++ app/src/main/java/local/kas/calc/MyButton.java
@@ -0,0 +1,11 @@
+package local.kas.calc;
+
+public class MyButton {
+    public String getView() {
+        return "1";
+    }
+
+    public String getCoordinats() {
+        return "1, 2";
+    }
+}
